925fe5a859f0ee71f7ad0bb3e10fe8af8273a29b
fermelli <luisfernandosalgadomiguez@gmail.com>
2024-10-28 23:22:55 -0400

    chore: instalacion y configuracion de TypeORM con MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GruposModule } from './grupos/grupos.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'finanzas_faciles_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    GruposModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
