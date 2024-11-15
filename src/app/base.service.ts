028ce0ffd7d7031120f56feeda857c82b325e197
fermelli <luisfernandosalgadomiguez@gmail.com>
2024-11-14 23:07:50 -0400

    feat: agregar logging de errores en BaseService

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -1,9 +1,13 @@
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, Logger } from '@nestjs/common';
 
 export class BaseService {
+  private readonly logger = new Logger(this.constructor.name);
+
   constructor() {}
 
   protected manejarErrores(error: any) {
+    this.logger.error(error);
+
     if (error.code === 'ER_DUP_ENTRY') {
       throw new BadRequestException('El registro ya existe');
     }
